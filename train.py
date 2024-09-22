2589ff3e022f558693cd6fc6bc82f839e67a3d57
Dmitry Sluch <dmitry.sluch@yandex.ru>
2024-09-22 18:06:57 +0300

    debug

--- train.py
+++ train.py
@@ -94,6 +94,7 @@ class SignalSampler:
         dataset,
         len=None,
         crop_size_sec: float = 5.0,
+        piece_size_sec: float = 5.0,
         min_rms_db: float | None = -38,
     ) -> None:
         """
@@ -105,6 +106,7 @@ class SignalSampler:
         self.config = config
         self.dataset = dataset
         self.crop_size_frames = int(crop_size_sec * self.config['sample_rate'] / self.config['hop_length'])
+        self.piece_size_frames = int(piece_size_sec * self.config['sample_rate'] / self.config['hop_length'])
         self.min_rms_db = min_rms_db
         self.sr = self.config['sample_rate']
         self.len = len
@@ -153,7 +155,7 @@ class SignalSampler:
         label_chunks: list[np.ndarray] = []
         duration_frames_remaining = self.crop_size_frames
         while duration_frames_remaining > 0:
-            audio, label = self._sample_from_single_file(duration_frames_remaining)
+            audio, label = self._sample_from_single_file(min(duration_frames_remaining, ))
             if self.min_rms_db is not None:
                 chunk_rms_db = eval_rms_db(audio)
                 if chunk_rms_db < self.min_rms_db:
@@ -204,7 +206,7 @@ def train(model_file, train, eval, run, device):
     model_state_dict = ckpt['model_state_dict']
 
     model = TranscriptionModel(config)
-    model.load_state_dict(model_state_dict)
+    # model.load_state_dict(model_state_dict)
     model.to(device)
 
     model_size = model.combined_fc.in_features
